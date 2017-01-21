5ff820b7267c1a48ad908f45327ddc37c2ddccc1
SteveGibbs <gibbsstephen@hotmail.com>
2017-01-21 12:20:48 +1100

    adding collapsible list for frequently asked questions

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,35 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+console.log("this is a test");
+
+// document.getElementById("test").onclick = function() {
+//   console.log("this worked!");
+// };
+
+$(document).on("turbolinks:load", function() {
+var acc = document.getElementsByClassName("accordion");
+console.log(acc);
+var i;
+for (i = 0; i < acc.length; i++) {
+  acc[i].onclick = function() {
+    console.log("testing");
+    this.classList.toggle("active");
+    /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+  };
+}
+});
+// // Get the button, and when the user clicks on it, execute myFunction
+// document.getElementById("myBtn").onclick = function() {myFunction()};
+//
+// /* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
+// function myFunction() {
+//   document.getElementById("myDropdown").classList.toggle("show");
+// }
