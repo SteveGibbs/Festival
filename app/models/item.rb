f410fff85a82be0ceb50a2020ffb0cfce3b1787a
SteveGibbs <gibbsstephen@hotmail.com>
2017-01-26 12:43:22 +1100

    building ticket purchase

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,4 +14,7 @@
 #
 
 class Item < ActiveRecord::Base
+    belongs_to :product
+    belongs_to :cart
+    belongs_to :order
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Product.destroy_all
+p1 = Product.create product_name: 'VIP Area', price: 600, product_desc1: 'Full access to VIP Area and Lounge'
+p2 = Product.create product_name: 'Premium', price: 250, product_desc1: 'Premium seating and priority access'
+p3 = Product.create product_name: 'General Admission', price: 100, product_desc1: 'General seating'
+p4 = Product.create product_name: 'Early Bird', price: 80, product_desc1: 'General seating'
